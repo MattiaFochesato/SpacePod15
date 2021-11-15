5a078327ca0e68af596ae6d60ebda8eca39ca1f4
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2021-11-15 09:53:53 +0100

    Initial Commit

---
+++ SpacePod15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpacePod15
+//
+//  Created by Mattia Fochesato on 15/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpacePod15/SpacePod15App.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePod15App.swift
+//  SpacePod15
+//
+//  Created by Mattia Fochesato on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SpacePod15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
